6101efebe4388a7a07001dbaf42c7f257eb176e5
Flaaj <michal.wolek96@gmail.com>
2020-09-12 18:14:36 +0200

    bigger grid, fixed some bugs with reset and clearWalls

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
+const gridRows = 40;
+const gridColumns = 40;
 const grid = document.getElementById("grid");
 const distances = [];
-for (let i = 0; i < 20; i++) {
-    distances.push([]);
-    for (let j = 0; j < 20; j++) {
-        distances[i].push(0);
-    }
-}
+
+
 let clicked = false;
 document.addEventListener("mousedown", function (e) {
+    // blokuje standardowy event dla wcisnięcia lewego przycisku - przeciąganie elementów
     e.preventDefault();
     clicked = true;
 });
 document.addEventListener("mouseup", function () {
     clicked = false;
 });
+
 const addElementToGrid = function (i, j) {
     const element = document.createElement("div");
     element.classList = ["el"];
@@ -46,11 +46,26 @@ const addElementToGrid = function (i, j) {
     grid.appendChild(element);
 };
 
-for (let i = 0; i < 20; i++) {
-    for (let j = 0; j < 20; j++) {
-        addElementToGrid(i, j);
+document.addEventListener("touchmove", function (e) {
+    e.preventDefault();
+    const touch = e.touches[0];
+    const element = document.elementFromPoint(touch.clientX, touch.clientY);
+    const [col, row] = element.id.split("x");
+    element.classList.toggle("el--wall");
+    distances[col][row] == 0
+        ? (distances[col][row] = -1)
+        : (distances[col][row] = 0);
+});
+
+const createGrid = () => {
+    for (let i = 0; i < gridRows; i++) {
+        distances.push([]);
+        for (let j = 0; j < gridColumns; j++) {
+            distances[i].push(0);
+            addElementToGrid(i, j);
+        }
     }
-}
+};
 
 const solveLabirynth = () => {
     const dirs = [
@@ -61,37 +76,33 @@ const solveLabirynth = () => {
     ];
 
     const visited = [[0, 0]];
-    let timerCounter = 0;
     distances[0][0] = 1;
     while (visited.length > 0) {
         [x, y] = visited.shift();
         dirs.forEach(([dx, dy]) => {
             if (
                 x + dx >= 0 &&
-                x + dx < 20 &&
+                x + dx < gridRows &&
                 y + dy >= 0 &&
-                y + dy < 20 &&
+                y + dy < gridColumns &&
                 distances[x + dx][y + dy] == 0
             ) {
                 distances[x + dx][y + dy] = distances[x][y] + 1;
-                // let idx = x + "x" + y;
-                // console.log(idx);
-                // document.getElementById(idx).style.backgroundColor = "red";
-
                 visited.push([x + dx, y + dy]);
             }
         });
     }
-    const path = [];
-    if (distances[19][19] > 0) {
-        [x, y] = [19, 19];
+
+    const path = [[gridRows - 1, gridColumns - 1]];
+    if (distances[gridRows - 1][gridColumns - 1] > 0) {
+        [x, y] = [gridRows - 1, gridColumns - 1];
         while (distances[x][y] > 1)
             dirs.forEach(([dx, dy]) => {
                 if (
                     x + dx >= 0 &&
-                    x + dx < 20 &&
+                    x + dx < gridRows &&
                     y + dy >= 0 &&
-                    y + dy < 20 &&
+                    y + dy < gridColumns &&
                     distances[x + dx][y + dy] == distances[x][y] - 1
                 ) {
                     path.push([x + dx, y + dy]);
@@ -99,40 +110,45 @@ const solveLabirynth = () => {
                 }
             });
     }
-
-    path.unshift([19, 19]);
     path.push([0, 0]);
+
+
+    let timerCounter = 0;
     path.reverse().forEach(([x, y]) => {
-        let idx = x + "x" + y;
+        let id = x + "x" + y;
         setTimeout(() => {
-            document.getElementById(idx).style.backgroundColor = "yellow";
-        }, 50 * timerCounter++);
+            document.getElementById(id).style.backgroundColor = "yellow";
+        }, 25 * timerCounter++);
     });
 };
 
-document.getElementById("solveLab").onclick = solveLabirynth;
-document.getElementById("resetDistances").onclick = function () {
-    for (let i = 0; i < 20; i++) {
-        for (let j = 0; j < 20; j++) {
+const resetDistances = () => {
+    for (let i = 0; i < gridRows; i++) {
+        for (let j = 0; j < gridColumns; j++) {
+            let idx = i + "x" + j;
+            document.getElementById(idx).style.backgroundColor = "";
             if (distances[i][j] > 0) {
-                let idx = i + "x" + j;
-                document.getElementById(idx).style.backgroundColor = "";
-
                 distances[i][j] = 0;
             }
         }
     }
     path = [];
 };
-document.getElementById("clearWalls").onclick = function () {
-    for (let i = 0; i < 20; i++) {
-        for (let j = 0; j < 20; j++) {
-            if (distances[i][j] == -1) {
+
+const clearWalls = () => {
+    for (let i = 0; i < gridRows; i++) {
+        for (let j = 0; j < gridColumns; j++) {
+            if (distances[i][j] < 1) {
                 distances[i][j] = 0;
-                const wall = document.getElementById(i + 'x' + j)
+                const wall = document.getElementById(i + "x" + j);
                 wall.classList.remove("el--wall");
             }
         }
     }
     path = [];
 };
+
+createGrid();
+document.getElementById("solveLab").onclick = solveLabirynth;
+document.getElementById("resetDistances").onclick = resetDistances;
+document.getElementById("clearWalls").onclick = clearWalls;
